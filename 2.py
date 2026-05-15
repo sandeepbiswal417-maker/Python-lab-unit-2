7010c5f7af9495c45a0af70c1ad766a6cf31fca4
Sandeep Biswal <sandeepbiswal417@gmail.com>
2026-05-15 11:22:29 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,8 @@
+fruits = ("Apple", "Banana", "Mango", "Orange")
+
+print("Tuple Elements:", fruits)
+print("First Element:", fruits[0])
+print("Length of Tuple:", len(fruits))
+
+for item in fruits:
+    print(item)
